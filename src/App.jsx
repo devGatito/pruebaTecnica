2d2b15a74950e00e1b3bde88650f2bd045427cf8
devgatito <eliasmisaelsotojimenez1@gmail.com>
2025-07-13 03:01:36 +0000

    prueba v2

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { useState } from "react";
 import { usoUsuarios } from "./hooks/usoUsuarios";
 import Modal from "./components/Modal";
 
-
 function App() {
   const { usuario, loading } = usoUsuarios();
   const [search, setSearch] = useState("");
@@ -18,17 +17,17 @@ function App() {
   if (loading) return <h1 className="text-center">Cargando</h1>;
   return (
     <>
-      <section className="m-6 wrapper mt-1 ">
-        <div className="p-6 " >
-          <input
-            type="text"
-            placeholder="Busca por nombre"
-            value={search}
-            onChange={(e) => setSearch(e.target.value)}
-            className="rounded-xl border-solid  w-full bg-[#E2E1E2] h-10 p-3.5 text-center"
-          ></input>
+      <section className="wrapper mx-auto px-4 sm:px-6 lg:px-8 py-4 max-w-7xl">
+        <input
+          type="text"
+          placeholder="Busca por nombre"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+          className="rounded-xl border-solid  w-full bg-[#E2E1E2] h-10 text-center mb-15"
+        ></input>
 
-          <table className="w-full  text-1xl m-60  ">
+        <div className="overflow-x-auto">
+          <table className="w-full  text-1xl  p-54  text-left  overflow-x-auto">
             <thead>
               <tr>
                 <th>Nombre</th>
@@ -38,23 +37,27 @@ function App() {
             </thead>
             <tbody>
               {filtradoUsuarios.map((usuario) => (
-                <tr key={usuario.id}>
-                  <td>{usuario.name}</td>
-                  <td>{usuario.email}</td>
-                  <td>{usuario.address.city}</td>
+                <tr key={usuario.id} className="bg-white shadow-sm hover:shadow-md transition-shadow">
+                  <td className="py-3 px-4 border-l-2 border-transparent hover:border-[#2B2D42]">{usuario.name}</td>
+                  <td className="py-3 px-4 border-l-2 border-transparent hover:border-[#2B2D42]">{usuario.email}</td>
+                  <td className="py-3 px-4 border-l-2 border-transparent hover:border-[#2B2D42]">{usuario.address.city}</td>
                   <td>
-                    <button onClick={() =>setUser(usuario) }>Ver</button>
+                    <button
+                      className="!px-4 !py-2 bg-[#2B2D42] text-white m-5"
+                      onClick={() => setUser(usuario)}
+                    >
+                      Ver
+                    </button>
                   </td>
                 </tr>
               ))}
             </tbody>
           </table>
-
-          {selectUser &&(
-            <Modal usuario={selectUser} onClose={() => setUser(null)} ></Modal>
-          )
-          }
         </div>
+
+        {selectUser && (
+          <Modal usuario={selectUser} onClose={() => setUser(null)}></Modal>
+        )}
       </section>
     </>
   );
--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,18 +1,29 @@
 import React from "react";
 
-const Modal = ({usuario, onClose}) => {
-  return <>
-  <div className="fixed inset-0 bg-blue-950 bg-opacity-50 flex items-center justify-center w-full max-w-96 max-h-60 ">
-    <div>
-        <h1>{usuario.name}</h1>
-        <p>{usuario.phone}</p>
-        <p>{usuario.company.name}</p>
+const Modal = ({ usuario, onClose }) => {
 
-        <button onClick={onClose} className="bg-red-500">Cerrar</button>
-    </div>
-  </div>
-  
-  </>;
+    
+  return (
+    <>
+      <div className="fixed inset-0 z-50 flex items-center justify-center bg-opacity-50  backdrop-blur-sm "> 
+        <div className=" bg-[#2B2D42] w-1/2 h-1/2 flex items-center justify-center text-gray-100 shadow-xl shadow-black/30 rounded-3xl">
+
+          <div className=" p-6 rounded  w-full max-w-sm text-center">
+            <h1 className="text-xl font-bold mb-2">{usuario.name}</h1>
+            <p className="mb-1">{usuario.phone}</p>
+            <p className="mb-4">{usuario.company.name}</p>
+
+            <button
+              onClick={onClose}
+              className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600 h-10 w-30"
+            >
+              Cerrar
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Modal;
